47136cd85c0e369e0e5789e21ffcacc751ac4193
jan <jan.meineke@mailbox.org>
2023-06-25 17:11:07 +0200

    react to button (sleep mode)

--- temp_mon/temp_mon.ino
+++ temp_mon/temp_mon.ino
@@ -13,17 +13,26 @@
 #include <WiFiNINA.h>
 #include <utility/wifi_drv.h>
 
+int buttonState = 0;
+int buttonSwitch = 1;
+
+const int buttonPin = 1;
+const int redLedPin = 25;
+const int greenLedPin = 26;
+const int blueLedPin = 27;
+
 DHT dht(2, DHT22);
 U8G2_SSD1306_128X64_ALT0_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // SSD1306 and SSD1308Z are compatible
  
 // U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);    //Low spped I2C
  
 void setup(void) {
-  WiFiDrv::pinMode(25, OUTPUT); // red
-  WiFiDrv::pinMode(26, OUTPUT); // green
-  WiFiDrv::pinMode(27, OUTPUT); // blue
+  WiFiDrv::pinMode(redLedPin, OUTPUT); // red
+  WiFiDrv::pinMode(greenLedPin, OUTPUT); // green
+  WiFiDrv::pinMode(blueLedPin, OUTPUT); // blue
+  WiFiDrv::pinMode(buttonPin, OUTPUT); // button
   Serial.begin(9600);
-  Serial.println(F("DHTxx test!"));
+  Serial.println(F("starting temperature and humidity monitor"));
   u8g2.begin();
   u8g2.clearBuffer();
   Wire.begin();
@@ -51,26 +60,43 @@ void loop(void) {
     delay(1000);  
     return;
   }
+
+  buttonState = digitalRead(buttonPin);
   
-  if (temp >= 32 || hum >= 70 ) {
-    WiFiDrv::analogWrite(25, 255);
-    WiFiDrv::analogWrite(26, 0);
-    WiFiDrv::analogWrite(27, 0);
+  Serial.println(buttonState);
+  if (buttonState == 1){
+    if (buttonSwitch == 1){
+      buttonSwitch = 0;
+    }else{
+      buttonSwitch = 1;
+    }
   }
-  else if(temp >= 30 || hum >= 60){
-    WiFiDrv::analogWrite(25, 128);
-    WiFiDrv::analogWrite(26, 128);
-    WiFiDrv::analogWrite(27, 0);
-  }
-  else if(temp >= 25 || hum >= 55){
-    WiFiDrv::analogWrite(25, 64);
-    WiFiDrv::analogWrite(26, 128);
-    WiFiDrv::analogWrite(27, 0);
-  }
-  else if(temp < 25 || hum < 55){
-    WiFiDrv::analogWrite(25, 0);
-    WiFiDrv::analogWrite(26, 128);
-    WiFiDrv::analogWrite(27, 0);
+
+  if (buttonSwitch == 0){
+    if (temp >= 32 || hum >= 70 ) {
+      WiFiDrv::analogWrite(25, 255);
+      WiFiDrv::analogWrite(26, 0);
+      WiFiDrv::analogWrite(27, 0);
+    }
+    else if(temp >= 30 || hum >= 60){
+      WiFiDrv::analogWrite(25, 128);
+      WiFiDrv::analogWrite(26, 128);
+      WiFiDrv::analogWrite(27, 0);
+    }
+    else if(temp >= 25 || hum >= 55){
+      WiFiDrv::analogWrite(25, 64);
+      WiFiDrv::analogWrite(26, 128);
+      WiFiDrv::analogWrite(27, 0);
+    }
+    else if(temp < 25 || hum < 55){
+      WiFiDrv::analogWrite(25, 0);
+      WiFiDrv::analogWrite(26, 128);
+      WiFiDrv::analogWrite(27, 0);
+    }
+  }else{
+      WiFiDrv::analogWrite(25, 0);
+      WiFiDrv::analogWrite(26, 0);
+      WiFiDrv::analogWrite(27, 0);
   }
   
   // print to monitor
@@ -89,5 +115,5 @@ void loop(void) {
   u8g2.drawStr(70,21,"%");
 
   u8g2.sendBuffer();
-  delay(500);  
+  delay(100);  
 }
